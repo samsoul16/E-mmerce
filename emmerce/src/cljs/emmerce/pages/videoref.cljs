f68b0b592db314bd093d98fadf0ef6596e899bc7
Sachin <sachinharpalani3795@gmail.com>
2017-08-18 20:51:47 +0530

    Added video-ref UI

--- emmerce/src/cljs/emmerce/pages/videoref.cljs
+++ emmerce/src/cljs/emmerce/pages/videoref.cljs
@@ -6,4 +6,43 @@
             [emmerce.ajax :refer [load-interceptors!]]
             [emmerce.handlers]
             [emmerce.subscriptions]
-            [reagent-material-ui.core :as ui]))
+            [reagent-material-ui.core :as ui]
+            [soda-ash.core :as sa]))
+
+
+(defn index []
+  []
+  [:div.centerFlex
+   [:div.middle
+    [sa/StepGroup
+     [sa/Step {:active true
+               :completed true}
+      [sa/Icon {:name "puzzle"}]
+      [sa/StepContent
+       [sa/StepTitle "Level 1"]]]
+     [sa/Step {:active true
+               :completed true}
+      [sa/Icon {:name "puzzle"}]
+      [sa/StepContent
+       [sa/StepTitle "Level 2"]]]
+     [sa/Step {:active true
+               :completed true}
+      [sa/Icon {:name "puzzle"}]
+      [sa/StepContent
+       [sa/StepTitle "Level 3"]]]
+     [sa/Step {:active true
+               :completed true}
+      [sa/Icon {:name "puzzle"}]
+      [sa/StepContent
+       [sa/StepTitle "Level 4"]]]]]
+   [:div.row
+    [sa/Embed {:id "O6Xo21L0ybE"
+               :source "youtube"
+               :className "video"}]]
+   [:div.flexButton
+    [sa/Button {:content "Prev"
+                :icon "left arrow"
+                :labelPosition "left"}]
+    [sa/Button {:content "Next"
+                :icon "right arrow"
+                :labelPosition "right"}]]])
