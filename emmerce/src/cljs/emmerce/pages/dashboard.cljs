3d386c51c33f64acb9c48518a2aef032ccb0ad8f
Sachin <sachinharpalani3795@gmail.com>
2017-08-19 05:48:28 +0530

    Added fast game

--- emmerce/src/cljs/emmerce/pages/dashboard.cljs
+++ emmerce/src/cljs/emmerce/pages/dashboard.cljs
@@ -10,5 +10,10 @@
 
 
 (defn dashboard []
-  [:div
-   [:h2 "DASHBOARD PAGE"]])
+  [:div.centerFlex.game
+   [ui/Paper {:zDepth 3}
+    [:iframe.gamestyle
+     {:src "https://www.gamestolearnenglish.com/htmlGames/fastEnglish/v12.1/index.html#site/personal"
+      :scrolling "no"
+      :height "550px"
+      :width "100%"}]]])
